4333947061f1b613c7739ce065d4448f2b4c6cfe
Surbhi Rathore <surbhi008@gmail.com>
2020-11-26 23:02:48 +0530

    changed base url

--- api/api-const.js
+++ api/api-const.js
@@ -1,6 +1,6 @@
 export const web_urls = {
-    BASE_URL : "http://event-api.vidhikaar.com/api/V1/Authentication/",
-    LOGIN_ENDPOINT : "Login",
+    BASE_URL : "http://event-api.vidhikaar.com/api/V1/",
+    LOGIN_ENDPOINT : "Authentication/Login",
     SIGNUP_ENDPOINT : "",
     PROFILE_ENDPOINT : "",
 }
